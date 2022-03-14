f709f687e7a5ca8735abd2bae61b5aaa46ce497c
SamuelSchnee <s.schnee13@gmail.com>
2022-03-14 17:06:03 -0500

    take 328

--- UnityProject/Assets/Scripts/ActionControllers/GameController.cs
+++ UnityProject/Assets/Scripts/ActionControllers/GameController.cs
@@ -7,12 +7,13 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Application.targetFrameRate = 100;
+
     }
 }
